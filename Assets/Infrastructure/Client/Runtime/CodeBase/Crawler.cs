ee873717836fc9201ca01a513537fcc79d061bdd
Vladyslav Synhaivskyi <ScvirelVlad@gmail.com>
2024-02-08 00:54:56 +0200

    Base Maze & Crawler

---
+++ Assets/Infrastructure/Client/Runtime/CodeBase/Crawler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MazeGeneration.Client.Runtime
+{
+    public class Crawler : Maze
+    {
+        protected override void Generate()
+        {
+            bool done = false;
+
+            int x = _widthMax / 2;
+            int z = _depthMax / 2;
+
+            Random random = new Random();
+
+            while (!done)
+            {
+                _map[x, z] = 0;
+
+                if (random.Next(2) == 0)
+                {
+                    x += random.Next(-1, 2);
+                }
+                else
+                {
+                    z += random.Next(-1, 2);
+                }
+
+                done |= x < 0 || x >= _widthMax || z < 0 || z >= _depthMax;
+            }
+        }
+    }
+}
---
+++ Assets/Infrastructure/Client/Runtime/CodeBase/Maze.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using Random = System.Random;
+
+namespace MazeGeneration.Client.Runtime
+{
+    public class Maze : MonoBehaviour
+    {
+        [SerializeField] protected int _widthMax = default;
+        [SerializeField] protected int _depthMax = default;
+
+        [SerializeField] protected byte[,] _map = default;
+
+        private void Awake()
+        {
+            _map = new byte[_widthMax, _depthMax];
+        }
+
+        private void Start()
+        {
+            InitialiseMap();
+            Generate();
+            DrawMap();
+        }
+
+        private void InitialiseMap()
+        {
+            //1 = wall
+            //0 = corridor
+            Random random = new Random();
+
+            for (int z = 0; z < _depthMax; z++)
+            {
+                for (int x = 0; x < _widthMax; x++)
+                {
+                    _map[x, z] = 1;
+                }
+            }
+        }
+
+        protected virtual void Generate()
+        {
+            Random random = new Random();
+
+            for (int z = 1; z < _depthMax - 1; z++)
+            {
+                for (int x = 1; x < _widthMax - 1; x++)
+                {
+                    _map[x, z] = (byte)random.Next(2);
+                }
+            }
+        }
+
+        private void DrawMap()
+        {
+            for (int z = 0; z < _depthMax; z++)
+            {
+                for (int x = 0; x < _widthMax; x++)
+                {
+                    if (_map[x, z] == 1)
+                    {
+                        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(x, 0, z);
+                    }
+                }
+            }
+        }
+    }
+}
